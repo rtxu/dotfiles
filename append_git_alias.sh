0d8a762dd79cae3f019002b797e5b44665455597
ruitao.xu <ruitao.xu@aliyun-inc.com>
2015-11-16 16:45:48 +0800

    add up alias for pull --rebase

--- append_git_alias.sh
+++ append_git_alias.sh
@@ -3,6 +3,7 @@
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.up 'pull --rebase'
 git config --global alias.unstage 'reset HEAD --'
 
 git config --global alias.last 'log -1 HEAD' 
